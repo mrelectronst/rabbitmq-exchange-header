f9c9f59b4ca3fd5ad372d2a0a6960f6220a75b95
mrelectronst <fhkaraman@outlook.com>
2022-04-15 15:55:34 +0300

    Add project files.

---
+++ ConsoleAppRabbitMQ/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using PublisherRabbitMQ;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri(""); //write AMQP URL
+
+string messageRead;
+
+do
+{
+    Console.Write("Please write your message for 50 times: ");
+    messageRead = Console.ReadLine();
+    if (messageRead != string.Empty && messageRead != null)
+    {
+        PublishMessage(messageRead);
+    }
+    else
+    {
+        Environment.Exit(0);
+    }
+
+} while (messageRead != null);
+
+void PublishMessage(string message)
+{
+    Random randomEnumLogName = new Random();
+
+    try
+    {
+        using (var connection = factory.CreateConnection())
+        {
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("exchange-header-logs", durable: true, type: ExchangeType.Headers);
+
+            Dictionary<string, object> header = new Dictionary<string, object>();
+
+            header.Add("format", "pdf");
+            header.Add("shape", "a4");
+
+            var property = channel.CreateBasicProperties();
+
+            property.Headers = header;
+
+            channel.BasicPublish("exchange-header-logs", string.Empty, property, Encoding.UTF8.GetBytes(message));
+
+            Console.WriteLine("All Logs are sended");
+        }
+    }
+    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+
+}
+
+
---
+++ SubscriberRabbitMQ/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri(""); //write AMQP URL
+
+using (var connection = factory.CreateConnection())
+{
+    var channel = connection.CreateModel();
+
+    channel.ExchangeDeclare("exchange-header-logs", durable: true, type: ExchangeType.Headers);
+
+    channel.BasicQos(0, 1, false);
+
+    var subscriber = new EventingBasicConsumer(channel);
+
+    string queueName = channel.QueueDeclare().QueueName;
+
+    Dictionary<string, object> header = new Dictionary<string, object>();
+
+    header.Add("format", "pdf");
+    header.Add("shape", "a4");
+    header.Add("x-match","all");
+
+    channel.QueueBind(queueName, "exchange-header-logs",string.Empty,header);
+
+    channel.BasicConsume(queueName, false, subscriber);
+
+    Console.WriteLine("Listening...");
+
+    subscriber.Received += (object? sender, BasicDeliverEventArgs e) =>
+    {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+        Thread.Sleep(1000);
+
+        Console.WriteLine($"Received Message : {message}");
+
+        //File.AppendAllText("logs"+ queueName + ".txt", message + "\n");
+
+        channel.BasicAck(e.DeliveryTag, false);
+    };
+
+    Console.ReadLine();
+}
